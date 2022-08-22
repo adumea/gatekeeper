ab91f0dbd8f62165a6603c6ab2d8a0cf07e8eac4
adumea <107037824+adumea@users.noreply.github.com>
2022-08-22 09:06:31 +0300

    Update index.php

--- packages/main/proxy/index.php
+++ packages/main/proxy/index.php
@@ -3,9 +3,9 @@ function main()
 {
 $url = $_GET['dst'] ?? "https://qr.modig.app/qrCode/get-code";
 $postData = $_GET;
-    var_dump($_GET);
-    var_dump($_POST);
 $test = json_encode($_POST);
+    var_dump($_POST);
+    var_dump($_GET);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -87,65 +87,4 @@ var_dump($output);
 
 //     return "";
 // }
-main();
-?>
-
-
-// require "config.php";
-
-// if(PROXY_SUBFOLDER != '')
-// 	$uri = str_replace(PROXY_SUBFOLDER, '',$_SERVER['REQUEST_URI']);
-
-// $url = MASKED_DOMAIN . $uri;
-
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_HEADER, 1);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FOLLOW_LOCATION); 
-// curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-
-// $cookies = "";
-// foreach($_COOKIE as $k=>$v)
-// 	$cookies .= $k.'='.$v.';';
-
-// curl_setopt($ch, CURLOPT_COOKIE, '"'.$cookies. '"');
-// $data = curl_exec($ch);
-// $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-// $header_len = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-// $header = substr($data, 0, $header_len);
-// $body = substr($data, $header_len);
-
-// curl_close($ch);
-
-// /* get cookies from curl'd data */
-// preg_match('/^Set-Cookie:\s*([^;]*)/mi', $data, $m);
-
-// if(isset($m[1]))
-// {
-// 	parse_str($m[1], $cookies);
-// 	foreach($cookies as $key => $value)
-// 	{
-// 		$expire = time()+3600*24*30;
-// 		setcookie($key,$value,$expire,'/',$_SERVER['HTTP_HOST']); // set cookies again
-// 	}
-// }
-
-
-// if($http_code == 301 || $http_code == 302)
-// 	header('Location: '. get_redirect_url($data));
-// else
-// {
-// 	echo $body;
-// }
-
-
-// function get_redirect_url($header) {
-//     if(preg_match('/^Location:\s+(.*)$/mi', $header, $m)) {
-//         return trim($m[1]);
-//     }
-
-//     return "";
-// }
-
 ?>
